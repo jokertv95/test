47b0650a2f15f4b4d0885af620c02862e7aae863
Algis <jokertv95@gmail.com>
2019-04-06 21:39:12 +0300

    Это мой первый комит

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let persone ={
+    name: "Babaj",
+    age:23,
+    isMarried:false,
+}
+
+console.log(persone["name"]);
+
+let arr=['plum.png','orange.png','apple.png'];
+
+console.log(arr[0]);
+
+//alert("Hello World")
+
+//let answer=confirm("Are you here?");
+//console.log(answer);
+
+//let answer = +promt("How old are you?" , "Yes");
+
+//console.log(typeof (answer));
+
+//console.log("arr"+" - object")
+//console.log(4+ " - object");
+
+let incr=10;
+    decr=10;
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+    console.log("2"===2);
+
+    let isCheked = true;
+        isClose = false;
+
+        console.log(isCheked || !isClose);
